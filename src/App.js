4d6a69bae937e9fd47d4afeebf8bcf80e93d07e6
anikmishra836 <anikmishra836@gmail.com>
2023-01-12 12:59:43 +0530

    added google and facebook login functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import Form from './login_signup_form/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Slider from './slider/Slider';
 import Login1 from './login_form_2';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import FacebookLoginComponent from './react-facebook-login';
+import ReactGoogleLogin from './react-google-login';
+
 
 function App() {
   return (
     <div>
-      <Slider />
+      <ReactGoogleLogin />
     </div>
   );
 }
---
+++ src/react-google-login/index.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
+
+function ReactGoogleLogin() {
+    const [profile, setProfile] = useState();
+    const clientId = '43634364208-56dpbe39cgo5f7n8lccem4sj9rg53h33.apps.googleusercontent.com';
+    useEffect(() => {
+        const initClient = () => {
+            gapi.client.init({
+                clientId: clientId,
+                scope: ''
+            });
+        };
+        gapi.load('client:auth2', initClient);
+    });
+
+    const onSuccess = (res) => {
+        localStorage.setItem('user', JSON.stringify(res?.profileObj));
+        
+    };
+    useEffect(() => {
+        let response = JSON.parse(localStorage.getItem('user'));
+        setProfile(response);
+        }, []);
+
+        const { name, googleId, imageUrl, email } = profile || {};
+
+    const onFailure = (err) => {
+        console.log('failed', err);
+    };
+
+    const logOut = () => {
+        localStorage.clear();
+    };
+
+    return (
+        <div>
+            <h2>React Google Login</h2>
+            <br />
+            <br />
+            {googleId ? (
+                <div>
+                    <img src={imageUrl} alt="user_image" />
+                    <h3>User Logged in</h3>
+                    <p>Name: {name}</p>
+                    <p>Email Address: {email}</p>
+                    <br />
+                    <br />
+                    <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
+                </div>
+            ) : (
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign in with Google"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy='single_host_origin'
+                    isSignedIn={true}
+                />
+            )}
+        </div>
+    );
+}
+export default ReactGoogleLogin;
